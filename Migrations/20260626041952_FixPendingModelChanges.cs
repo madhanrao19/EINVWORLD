a27e10fb27a1787bc891c5ff2e67904173c66295
madhanrao19 <92283219+madhanrao19@users.noreply.github.com>
2026-06-26 22:29:48 +0800

    update

---
+++ Migrations/20260626041952_FixPendingModelChanges.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eInvWorld.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPendingModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SyncJobs_Status_NextRunAtUtc",
+                table: "SyncJobs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InvoiceHeaders_LHDNStatusId",
+                table: "InvoiceHeaders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_SyncJobs_Status_NextRunAtUtc",
+                table: "SyncJobs",
+                columns: new[] { "Status", "NextRunAtUtc" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InvoiceHeaders_LHDNStatusId",
+                table: "InvoiceHeaders",
+                column: "LHDNStatusId");
+        }
+    }
+}
